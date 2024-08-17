a2acb7417485dd5b02bb8bb2b8c3c872c150b09e
Ben <8870073+ben-dodd@users.noreply.github.com>
2024-08-17 22:57:30 +1200

    Added search styling

--- components/layout/search.tsx
+++ components/layout/search.tsx
@@ -1,13 +1,30 @@
+import { IconSearch, IconX } from '@tabler/icons-react'
+
 const Search = ({ value, setValue }) => {
+  const handleSearch = (e) => setValue(e.target.value)
+  const clearInput = () => setValue('')
   return (
-    <div>
-      <input
-        type="text"
-        className="w-full p-1 border border-gray-200 mb-8"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        placeholder="Search.."
-      />
+    <div
+      className={`flex flex-1 my-1 p-1 rounded-lg justify-between items-center ring-1 ring-gray-400 w-auto bg-gray-100 hover:bg-gray-200 ${
+        value && 'bg-blue-100 hover:bg-blue-200'
+      }`}
+    >
+      <div className="flex items-center flex-1">
+        <div className="pl-3 pr-1">
+          <IconSearch />
+        </div>
+        <input
+          className="w-full px-2 text-sm outline-none bg-transparent text-xl"
+          value={value || ''}
+          onChange={handleSearch}
+          placeholder="Search..."
+        />
+      </div>
+      <div className="text-xs mr-2 hover:text-gray-500">
+        <button onClick={clearInput}>
+          <IconX />
+        </button>
+      </div>
     </div>
   )
 }
